53c07edb196a2867df71fc5172c29fc7dc876644
Steve Phelps <79sphelps@gmail.com>
2024-06-18 13:58:58 -0700

    compressing banner image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 // import headerImg from "../assets/img/header-img.svg";
-import headerImg from "../assets/img/header-img.webp";
+// import headerImg from "../assets/img/header-img.webp";
+import headerImg from "../assets/img/header-img-min.png";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
