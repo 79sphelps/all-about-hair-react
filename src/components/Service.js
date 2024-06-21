14d105cb502da9981c7ffa8d3c5810e94bb69ed9
Steve Phelps <79sphelps@gmail.com>
2024-06-21 11:55:33 -0700

    more service detail css updates

--- src/components/Service.js
+++ src/components/Service.js
@@ -52,28 +52,31 @@ export const Service = () => {
       {/* <Container style={{ marginTop: "100px" }}> */}
       <Container>
       <button 
-        className="animate__animated animate__backInLeft" 
+        className="service-button animate__animated animate__backInLeft" 
         onClick={() => { navigate('/')}} 
-        style={{ border: "1px solid rgba(255, 255, 255, 0.5)",
-            marginTop: "25px",
-            marginBottom: "50px",
-            padding: "25px",
-            borderRadius: "25px",
-            width: "20%",
-            fontColor: "white !important;" 
-                }} ><span style={{ fontColor: "white !important;" }}>Back to Home</span> <ArrowRightCircle size={25} /></button>
+        style={{  border: "1px solid rgba(255, 255, 255, 0.5)",
+          // marginTop: "25px",
+          // marginBottom: "50px",
+          margin: "25px",
+          padding: "25px",
+          borderRadius: "25px",
+        }} ><span>Back to Home</span> <ArrowRightCircle size={25} /></button>
             
-        <Row className="align-items-center" style={{ border: "1px solid rgba(255, 255, 255, 0.5)",
-                                  marginTop: "25px",
-                                  marginBottom: "50px",
-                                  padding: "25px",
-                                  borderRadius: "25px" }}>
+        <Row className="align-items-center" 
+          style={{ border: "1px solid rgba(255, 255, 255, 0.5)",
+                  // marginTop: "25px",
+                  // marginBottom: "50px",
+                  margin: "25px",
+                  paddingLeft: "25px",
+                  paddingBottom: "25px",
+                  paddingTop: "25px",
+                  borderRadius: "25px" }}>
 
             <Col lg={4} md={4} sm={12}>
             <h2>Service: { serviceDetails.title }</h2>
             
             <div style={{ alignContent: "center", alignItems: "center"}}>
-                <img src={ require('../' + serviceDetails.image) } style={{ width: "50%"}}></img>
+                <img src={ require('../' + serviceDetails.image) } style={{ width: "50%" }}></img>
             </div>
         
             </Col>
@@ -87,8 +90,9 @@ export const Service = () => {
                     serviceDetails.pricing.map((service, idx) => {
                     return (
                         <Row key={service._id}  className="align-items-center" style={{ border: "1px solid rgba(255, 255, 255, 0.5)",
-                        marginTop: "25px",
-                        marginBottom: "50px",
+                        // marginTop: "25px",
+                        // marginBottom: "50px",
+                        margin: "25px",
                         padding: "25px",
                         borderRadius: "25px" }}>
                         <div>Type: {service.type}</div>
